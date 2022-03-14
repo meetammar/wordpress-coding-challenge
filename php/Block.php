5f4686652827626a3a4a33e145aa74b61743e7d7
Ammar Ilyas <39131385+meetammar@users.noreply.github.com>
2022-03-15 04:47:45 +0500

    Upgrading block.php
    - fixed errors
    - handled security checks
    - modified code to improve scalability

--- php/Block.php
+++ php/Block.php
@@ -63,63 +63,90 @@ class Block {
 	 * @return string The markup of the block.
 	 */
 	public function render_callback( $attributes, $content, $block ) {
-		$post_types = get_post_types(  [ 'public' => true ] );
-		$class_name = $attributes['className'];
+
+		$post_types = get_post_types( [ 'public' => true ] );
+		$class_name = isset( $attributes['className'] ) ? $attributes['className'] : '';
 		ob_start();
-
 		?>
-        <div class="<?php echo $class_name; ?>">
-			<h2>Post Counts</h2>
+		<div class="<?php echo esc_attr( $class_name ); ?>">
+			<h2><?php _e( 'Post Counts', 'site-counts' ); ?></h2>
 			<ul>
-			<?php
-			foreach ( $post_types as $post_type_slug ) :
-                $post_type_object = get_post_type_object( $post_type_slug  );
-                $post_count = count(
-                    get_posts(
-						[
-							'post_type' => $post_type_slug,
-							'posts_per_page' => -1,
-						]
-					)
-                );
-
+				<?php
+				if ( ! empty( $post_types ) ) :
+					foreach ( $post_types as $post_type_slug ) :
+						$post_type_object = get_post_type_object( $post_type_slug );
+						$post_count       = count(
+							get_posts(
+								[
+									'post_type'      => $post_type_slug,
+									'posts_per_page' => -1,
+								]
+							)
+						);
+						?>
+						<li>
+							<?php
+							if ( $post_count > 0 ) {
+								echo wp_sprintf(
+									/* translators: 1: Total count, 2:3: String text for Post/Taxonomy */
+									_n(
+										'There is only %1$l %2$s.',
+										'There are %1$l %3$s.',
+										esc_attr( $post_count ),
+										'site-counts'
+									),
+									esc_attr( number_format_i18n( $post_count ) ),
+									esc_attr( $post_type_object->labels->singular_name ),
+									esc_attr( $post_type_object->labels->name ),
+								);
+							} else {
+								echo wp_sprintf(
+									/* translators: %s: String text for Post/Taxonomy */
+									esc_attr__( 'There are no %s', 'site-counts' ),
+									esc_attr( $post_type_object->labels->name )
+								);
+							}
+							?>
+						</li>
+						<?php
+					endforeach;
+				endif;
 				?>
-				<li><?php echo 'There are ' . $post_count . ' ' .
-					  $post_type_object->labels->name . '.'; ?></li>
-			<?php endforeach;	?>
-			</ul><p><?php echo 'The current post ID is ' . $_GET['post_id'] . '.'; ?></p>
+			</ul>
+			<p>
+				<?php
+				echo wp_sprintf(
+					esc_html__( 'The current post ID is %l.', 'site-counts' ),
+					get_the_ID()
+				);
+				?>
+			</p>
 
 			<?php
-			$query = new WP_Query(  array(
-				'post_type' => ['post', 'page'],
-				'post_status' => 'any',
-				'date_query' => array(
-					array(
-						'hour'      => 9,
-						'compare'   => '>=',
-					),
-					array(
-						'hour' => 17,
-						'compare'=> '<=',
-					),
-				),
-                'tag'  => 'foo',
-                'category_name'  => 'baz',
-				  'post__not_in' => [ get_the_ID() ],
-			));
+			$query = new WP_Query(
+				[
+					'post_type'      => 'post',
+					'post_status'    => 'any',
+					'tag'            => 'foo',
+					'category_name'  => 'baz',
+					'post__not_in'   => [ get_the_ID() ],
+					'posts_per_page' => 5,
+				]
+			);
 
 			if ( $query->have_posts() ) :
 				?>
-				 <h2>5 posts with the tag of foo and the category of baz</h2>
-                <ul>
-                <?php
-
-                 foreach ( array_slice( $query->posts, 0, 5 ) as $post ) :
-                    ?><li><?php echo $post->post_title ?></li><?php
-				endforeach;
+				<h2><?php _e( '5 posts with the tag of foo and the category of baz', 'site-counts' ); ?></h2>
+				<ul>
+					<?php
+					foreach ( $query->posts as $post ) :
+						echo '<li>' . esc_html( $post->post_title ) . '</li>';
+					endforeach;
+					?>
+				</ul>
+				<?php
 			endif;
-		 	?>
-			</ul>
+			?>
 		</div>
 		<?php
 
